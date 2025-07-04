5a5dc554acdbaf53716fa200dbf6d3c74f180e62
956065487 <956065487@qq.com>
2025-07-04 20:15:39 +0800

    增加工具类注释

--- script/Utils.cs
+++ script/Utils.cs
@@ -4,21 +4,34 @@ namespace CardGame.script;
 
 public static class Utils
 {
+    /**
+     * node：需要反应错误的节点，一般可直接填 this ，表示当前节点
+     * 打印错误信息，附带类型和节点名，
+     */
     public static void PrintErr(Node node,string msg)
     {
         GD.PrintErr("[" + node.GetType().Name + "]" + node.GetName() + " 错误: " + msg);
     }
     
+    /**
+     * 方法重载，单纯在GD printErr基础上添加“错误：”
+     */
     public static void PrintErr(string msg)
     {
         GD.PrintErr(" 错误： " + msg);
     }
     
+    /**
+     * GD.Print
+     */
     public static void Print(string msg)
     {
         GD.Print(msg);
     }
     
+    /**
+     * GD.Print,附带打印节点类型，节点名
+     */
     public static void Print(Node node,string msg)
     {
         GD.Print("[" + node.GetType().Name + "]" + node.GetName() + " : " + msg);
