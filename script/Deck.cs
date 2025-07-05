592b2af8418c904d51df39c3df786c6ce22b1e9f
956065487 <956065487@qq.com>
2025-07-05 21:22:59 +0800

    卡组逻辑实现完成

--- script/Deck.cs
+++ script/Deck.cs
@@ -10,27 +10,35 @@ public partial class Deck : Node2D
 
 	private List<String> _playerDeck = ["Knight", "Knight", "Knight"];
 
+	private RichTextLabel _numLabel;	// 显示卡组数量用
+
 	#endregion
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Area2D area2D = GetNode<Area2D>("Area2D");
 		area2D.CollisionLayer = Constant.LAYER_DECK;
+		_numLabel = GetNode<RichTextLabel>("NumberLabel");
+		_numLabel.Text = _playerDeck.Count.ToString();
 	}
 
 	public void DrawCard()
 	{
 		if (_playerDeck.Count == 0)
 		{
+			// 重置隐藏等
 			CollisionShape2D deckCollisionShape2D = 
 				GetNodeOrNull<CollisionShape2D>("Area2D/CollisionShape2D");
 			deckCollisionShape2D.Disabled = true;
 			Sprite2D deckSprite2D = GetNodeOrNull<Sprite2D>("Sprite2D");
 			deckSprite2D.Visible = false;
+			_numLabel.Visible = false;
 			return;
 		}
 		var cardDraw = _playerDeck[0];
 		_playerDeck.Remove(cardDraw);
+		
+		_numLabel.Text = _playerDeck.Count.ToString();
 		
 		GD.Print("DrawCard");
 		PackedScene cardScene = GD.Load<PackedScene>(Constant.CARD_SCENE_PATH);
